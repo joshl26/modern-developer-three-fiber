52eb998bb1c726e03ce9e566e5ef3db58ed50efd
Joshua Lehman <joshlehman.dev@gmail.com>
2023-01-09 16:30:18 -0500

    commit

--- EP2/src/components/Experience.js
+++ EP2/src/components/Experience.js
@@ -1,11 +1,29 @@
 import { OrbitControls } from "@react-three/drei";
+import Model from "./Model";
 
 const Experience = () => {
   return (
     <mesh>
       <OrbitControls />
-      <boxBufferGeometry />
-      <meshNormalMaterial />
+      <ambientLight />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <group position={[0, -1, 0]}>
+        <Model />
+      </group>
+
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeBufferGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
     </mesh>
   );
 };
